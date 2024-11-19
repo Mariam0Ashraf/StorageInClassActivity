b553691af80ff3a168fd83fe34195b03f859085f
mariam0ashraf <mariam00ashraf@gmail.com>
2024-11-19 15:06:17 -0500

    comic

--- app/src/main/java/com/example/networkapp/MainActivity.kt
+++ app/src/main/java/com/example/networkapp/MainActivity.kt
@@ -1,23 +1,19 @@
 package com.example.networkapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.squareup.picasso.Picasso
 import org.json.JSONObject
-
-// TODO (1: Fix any bugs)
-// TODO (2: Add function saveComic(...) to save comic info when downloaded
-// TODO (3: Automatically load previously saved comic when app starts)
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,47 +23,74 @@ class MainActivity : AppCompatActivity() {
     lateinit var numberEditText: EditText
     lateinit var showButton: Button
     lateinit var comicImageView: ImageView
+    lateinit var comicPlaceholderTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         requestQueue = Volley.newRequestQueue(this)
-
-        titleTextView = findViewById<TextView>(R.id.comicTitleTextView)
-        descriptionTextView = findViewById<TextView>(R.id.comicDescriptionTextView)
-        numberEditText = findViewById<EditText>(R.id.comicNumberEditText)
-        showButton = findViewById<Button>(R.id.showComicButton)
-        comicImageView = findViewById<ImageView>(R.id.comicImageView)
+        titleTextView = findViewById(R.id.comicTitleTextView)
+        descriptionTextView = findViewById(R.id.comicDescriptionTextView)
+        numberEditText = findViewById(R.id.comicNumberEditText)
+        showButton = findViewById(R.id.showComicButton)
+        comicImageView = findViewById(R.id.comicImageView)
+        comicPlaceholderTextView = findViewById(R.id.comicNumberEditText)
 
         showButton.setOnClickListener {
-            downloadComic(numberEditText.text.toString())
+            val comicId = numberEditText.text.toString()
+            if (comicId.isNotEmpty()) {
+                downloadComic(comicId)
+            } else {
+                Toast.makeText(this, "Please enter a comic number", Toast.LENGTH_SHORT).show()
+            }
         }
-
     }
 
-    // Fetches comic from web as JSONObject
-    private fun downloadComic (comicId: String) {
+    private fun downloadComic(comicId: String) {
         val url = "https://xkcd.com/$comicId/info.0.json"
-        requestQueue.add (
-            JsonObjectRequest(url
-                , {showComic(it)}
-                , {}
-            )
+        showLoadingPlaceholder()
+
+        val request = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            { response ->
+                showComic(response)
+            },
+            { error ->
+                comicPlaceholderTextView.apply {
+                    visibility = View.VISIBLE
+                }
+                Toast.makeText(this, "Error loading comic", Toast.LENGTH_SHORT).show()
+            }
         )
+        requestQueue.add(request)
     }
 
-    // Display a comic for a given comic JSON object
-    private fun showComic (comicObject: JSONObject) {
+    private fun showComic(comicObject: JSONObject) {
+        comicPlaceholderTextView.visibility = View.GONE
+
         titleTextView.text = comicObject.getString("title")
         descriptionTextView.text = comicObject.getString("alt")
-        Picasso.get().load(comicObject.getString("img")).into(comicImageView)
+
+        Picasso.get()
+            .load(comicObject.getString("img"))
+            .into(comicImageView, object : com.squareup.picasso.Callback {
+                override fun onSuccess() {
+                    comicPlaceholderTextView.visibility = View.GONE
+                }
+
+                override fun onError(e: Exception?) {
+                    comicPlaceholderTextView.apply {
+                        visibility = View.VISIBLE
+                    }
+                }
+            })
     }
 
-    // Implement this function
-    private fun saveComic(comicObject: JSONObject) {
-
+    private fun showLoadingPlaceholder() {
+        comicPlaceholderTextView.apply {
+            visibility = View.VISIBLE
+            text = "Loading comic..."
+        }
     }
-
-
 }
